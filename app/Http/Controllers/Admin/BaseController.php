efab265c7f3c832dcc3475d778691abbf022fc45
fuyi <1715501806@qq.com>
2017-12-27 00:24:00 +0800

    基础控制器

---
+++ app/Http/Controllers/Admin/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2017/12/26
+ * Time: 23:12
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class BaseController
+{
+
+}
