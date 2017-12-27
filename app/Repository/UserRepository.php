82b664c27aaa4c5a165f234b2eae105d082d807b
fuyi <1715501806@qq.com>
2017-12-28 00:37:59 +0800

    实现模型常用方法接口

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -9,7 +9,58 @@
 namespace App\Repository;
 
 
-class UserRepository
-{
+use App\Model\User;
 
+/**
+ * Class UserRepository
+ * @package App\Repository
+ */
+class UserRepository implements RepositoryInterface {
+
+    protected $userModel;
+
+    public function __construct(User $userModel){
+        $this->userModel = $userModel;
+    }
+
+    public function all(Array $where,$columns = array('*'))
+    {
+        return $this->userModel->where($where)->get($columns);
+    }
+
+    public function paginate(Array $where,$perPage = 15, $columns = array('*'))
+    {
+        return $this->userModel->where($where)->paginate($perPage,$columns);
+    }
+
+    public function create(array $data)
+    {
+        return $this->userModel->create($data);
+    }
+
+    public function update(array $data, $id,$attribute="id")
+    {
+        return $this->userModel->where($attribute, '=', $id)->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->userModel->destroy($id);
+    }
+
+    public function find($id, $columns = array('*'))
+    {
+        return $this->userModel->find($id, $columns);
+    }
+
+    /**
+     * @param $attribute
+     * @param $value
+     * @param array $columns
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
+    public function findBy($attribute, $value, $columns = array('*'))
+    {
+        return $this->userModel->where($attribute, '=', $value)->first($columns);
+    }
 }
