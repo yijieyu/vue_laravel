939a4edf3b115119e1e4efcf0dc7f2f7a7fb1997
fuyi <1715501806@qq.com>
2017-12-28 00:39:25 +0800

    用户login逻辑service

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -9,7 +9,27 @@
 namespace App\Service;
 
 
-class UserService
-{
+use App\Repository\UserRepository;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
 
+class UserService {
+
+    protected $userRepository;
+    public function __construct(UserRepository $userRepository){
+        $this->userRepository = $userRepository;
+    }
+
+    public function login($credentials){
+        try{
+            if(!$token = JWTAuth::attempt($credentials)){
+                return ['error' => 'invalid_credentials','code'=>401];
+            }
+        }catch (JWTException $e){
+            return ['error' => 'could_not_create_token','code'=>500];
+        }
+
+        $name = $this->userRepository->findBy('email',$credentials['email'],['name'])['name'];
+        return compact('token', 'name');
+    }
 }
