f667d341d65517fd6f5d669fde3dc975d4637d0b
fuyi <1715501806@qq.com>
2017-12-28 00:37:03 +0800

    定义模型常用方法接口

--- app/Repository/RepositoryInterface.php
+++ app/Repository/RepositoryInterface.php
@@ -9,7 +9,13 @@
 namespace App\Repository;
 
 
-interface RepositoryInterface
-{
+interface RepositoryInterface{
 
+    public function all(Array $where,$columns = array('*'));
+    public function paginate(Array $where,$perPage = 15, $columns = array('*'));
+    public function create(array $data);
+    public function update(array $data, $id);
+    public function delete($id);
+    public function find($id, $columns = array('*'));
+    public function findBy($field, $value, $columns = array('*'));
 }
