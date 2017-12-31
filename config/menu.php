3dc20bc00269088d413ed72b71fbe5432a3109a0
fuyi <1715501806@qq.com>
2017-12-31 11:27:54 +0800

    添加菜单配置配置

---
+++ config/menu.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2017/12/31
+ * Time: 11:19
+ */
+
+return [
+    0   =>  [
+        'parent_name'  => '管理员管理',
+        'child'  => [
+            ['name' => '角色管理','path' => '/system/role/list','icon'=>'glyphicon glyphicon-flag'],
+            ['name' => '权限管理','path' => '/system/auth/list','icon'=>'glyphicon glyphicon-flag'],
+            ['name' => '管理员列表','path' => '/system/user/list','icon'=>'glyphicon glyphicon-user']
+        ]
+    ],
+];
