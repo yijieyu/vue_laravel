f5c28937c98ae633a83fd792ad1ca7626171091e
fuyi <1715501806@qq.com>
2017-12-31 18:19:53 +0800

    添加自定义样式

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,7 @@
 <!--    <meta name="csrf-token" content="{!! csrf_field() !!}">-->
     <title>Laravel & Vue Blog</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('admin/css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/css/base.css') }}">
 </head>
 <body>
 <div id="app"></div>
