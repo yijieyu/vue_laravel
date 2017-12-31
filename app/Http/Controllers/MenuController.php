da60d6a1a5ccaf569b1d3a49e0b2226849be10ba
fuyi <1715501806@qq.com>
2017-12-31 11:28:26 +0800

    添加系统配置控制器

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2017/12/31
+ * Time: 11:27
+ */
+
+namespace App\Http\Controllers;
+
+
+class MenuController extends Controller {
+
+}
