7fb7708db1d9a919b990d3bb1612f2f0533998b9
fuyi <1715501806@qq.com>
2018-01-01 13:34:27 +0800

    修改菜单栏名称的字段名

--- config/menu.php
+++ config/menu.php
@@ -10,9 +10,9 @@ return [
     0   =>  [
         'title'  => '管理员管理',
         'child'  => [
-            ['title' => '角色管理','path' => '/system/role/list','icon'=>'glyphicon glyphicon-flag'],
-            ['title' => '权限管理','path' => '/system/auth/list','icon'=>'glyphicon glyphicon-flag'],
-            ['title' => '管理员列表','path' => '/system/user/list','icon'=>'glyphicon glyphicon-user']
+            ['title' => '角色管理','path' => '/system/role-list','icon'=>'glyphicon glyphicon-flag'],
+            ['title' => '权限管理','path' => '/system/auth-list','icon'=>'glyphicon glyphicon-flag'],
+            ['title' => '管理员列表','path' => '/system/user-list','icon'=>'glyphicon glyphicon-user']
         ]
     ],
 ];
