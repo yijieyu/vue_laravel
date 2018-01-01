71687d030989fb4262d2e089e0e752f2a78aff8a
fuyi <1715501806@qq.com>
2018-01-01 13:32:14 +0800

    根据用户id获取单个用户，以及保存用户数据

--- app/Http/Controllers/Admin/SystemUserController.php
+++ app/Http/Controllers/Admin/SystemUserController.php
@@ -11,6 +11,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Service\SystemUserService;
+use Illuminate\Http\Request;
 
 class SystemUserController extends Controller {
 
@@ -23,5 +24,17 @@ class SystemUserController extends Controller {
     public function listByPage(){
         $list = $this->systemUserService->paginate();
         return $list;
+    }
+
+    public function getOneUser(Request $request){
+        $userID = $request->only('id');
+        $user = $this->systemUserService->getOneUserByID($userID);
+
+        return $user;
+    }
+
+    public function saveUser(Request $request){
+        $data = $request->only('mail','phone','desc','id');
+        return $this->systemUserService->updateUser($data);
     }
 }
