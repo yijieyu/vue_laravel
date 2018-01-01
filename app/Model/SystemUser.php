431b8f8820c8ec2b3542a957e2cf7e6d619e3a87
fuyi <1715501806@qq.com>
2018-01-01 13:29:37 +0800

    禁止使用自动更新，允许获取密码字段

--- app/Model/SystemUser.php
+++ app/Model/SystemUser.php
@@ -15,12 +15,13 @@ class SystemUser extends Model{
 
     protected $table = 'system_user';
     protected $primaryKey = 'id';
+    public $timestamps = false;
 
     protected $fillable = [
         'username', 'mail', 'password',
     ];
 
     protected $hidden = [
-        'password',
+
     ];
 }
